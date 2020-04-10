a5613dbcb1379de9c96cabbfb5a63e3e573e5cd3
irshadmhd46 <irshadmhd46@gmail.com>
2020-04-10 16:37:27 +0530

    fos bundle integrated

---
+++ src/AppBundle/Form/RegistrationFormType.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use FOS\UserBundle\Form\Type\RegistrationFormType as BaseRegistrationFormType;
+
+/**
+ * Description of RegistrationFormType
+ *
+ * @author irshad
+ */
+class RegistrationFormType extends AbstractType {
+
+    public function buildForm(FormBuilderInterface $builder, array $options) {
+        $builder->add('phoneNo');
+    }
+
+    public function getParent() {
+        return BaseRegistrationFormType::class;
+    }
+
+}
---
+++ src/UserBundle/Services/UserServices.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of UserServices
+ *
+ * @author irshad
+ */
+class UserServices {
+    //put your code here
+}
